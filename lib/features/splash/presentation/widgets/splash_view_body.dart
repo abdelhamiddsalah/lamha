529871332abc78524fbf30ddf990c25c4f883a68
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2025-05-14 22:24:28 +0300

    add splash

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lamha/core/styles/app_colors.dart';
-import 'package:lamha/core/widgets/custom_button.dart';
-import 'package:lamha/core/widgets/custom_textformfield.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -9,14 +6,7 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: CustomButton(color: AppColors.mainColor, width: double.infinity, text: 'text', textcolor: Colors.white,),
-          ),
-          CustomTextformfield(controller: null, hinttext: 'hinttext', suffixIcon: null,)
-        ],
-      ),
+      
     );
   }
 }
